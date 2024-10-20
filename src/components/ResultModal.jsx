761fa52d8b5a30f5b43a5c82eb169000cf162078
Rasreal <ersultan.t16@gmail.com>
2024-10-21 02:06:46 +0500

    // eslint-dasable react prop types

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-
+/* eslint-disable react/prop-types */
 import { useRef, forwardRef, useImperativeHandle } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
